a81b0d8d3018aab2e947e8e58dc8231036458301
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-10-03 11:53:27 +0530

    Setting environment Variable

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.js"
 
@@ -10,7 +11,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // allows us to accept JSON data in the body
 
-app.use("/api/products",productRoutes)
+const __dirname = path.resolve();
+
+app.use("/api/products",productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectDB()
